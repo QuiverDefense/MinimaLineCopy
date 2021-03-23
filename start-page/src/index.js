c2fefcb29b13196e9eb1ac07b8106274c4c44eb8
Mira Mohammad <mohammira@gmail.com>
2021-03-23 13:54:22 +0800

    4.2 Start Page

---
+++ start-page/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Buttons from './components/button';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Buttons />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
