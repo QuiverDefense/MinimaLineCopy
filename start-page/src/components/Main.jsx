8e3ac7bb7ffafb4d88e079d4eb25fbed96fa29f7
Mira Mohammad <mohammira@gmail.com>
2021-04-01 21:13:36 +0800

    Deleted and renamed some files

--- start-page/src/components/Main.jsx
+++ start-page/src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import './style.css'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,22 +7,21 @@ import Col from 'react-bootstrap/Col';
 import { FiArrowRightCircle } from "react-icons/fi";
 import logo from './MINIMALINE.png';
 
-class Buttons extends Component {
-    constructor(){
-        super();
-        this.state = {
-            bgColor: 'outline-secondary',  
-            blueButtonId: null
-        }
-       this.changeColor = this.changeColor.bind (this);
-    }
-    
-    changeColor(id){
-        this.setState({
-            blueButtonId: this.state.blueButtonId === id ? null : id
-        })
-    }
-
+class Main extends Component {
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         bgColor: 'outline-secondary',  
+    //         blueButtonId: null
+    //     }
+    //    this.changeColor = this.changeColor.bind (this);
+    // }
+    // changeColor(id){
+    //     this.setState({
+    //         blueButtonId: this.state.blueButtonId === id ? null : id
+    //     })
+    // }
+    // variant = {this.state.blueButtonId === 0 ? "warning": "outline-secondary"}
     render() { 
         return ( 
             <div className="white-box shadow-lg p-3 mb-5 bg-white rounded">
@@ -33,41 +32,39 @@ class Buttons extends Component {
                             <img className="logo" src={logo} alt="MinimaLine logo"/>
                         </Col>
                     </Row>
+
                     {/* REGULAR OR PRIORITY? */}
                     <Row sm md lg="auto">
                         <Col sm md lg="auto" className="m-auto">
                             <Button
-                                onClick={() => {this.changeColor(0)}}
-                                variant = {this.state.blueButtonId === 0 ? "warning": "outline-secondary"}
+                                variant = "outline-secondary"
                                 size="lg"
-                                style={{width: 120, marginTop: 20}}
+                                style={{width: 120}}
                                 className="mx-3 mb-5">
                                 Regular
                             </Button>{' '}
                             <Button
-                                onClick={() => {this.changeColor(1)} }
-                                variant = {this.state.blueButtonId === 1 ? "warning": "outline-secondary"}
+                                variant = "outline-secondary"
                                 size="lg"
-                                style={{width: 120, marginTop: 20}}
+                                style={{width: 120}}
                                 className="mx-3 mb-5">
                                     Priority
                             </Button>{' '}
                         </Col>
                     </Row>
+                    
                     {/* DINE IN OR TAKE OUT? */}
                     <Row sm md lg="auto">
                         <Col sm md lg="auto" className="m-auto">
                             <Button
-                                onClick={() =>{this.changeColor(3)}}
-                                variant = {this.state.blueButtonId === 3 ? "warning": "outline-secondary"}
+                                variant = "outline-secondary"
                                 size="lg"
                                 style={{width: 120}}
                                 className="mx-3 mb-5">
                                     Dine In
                             </Button>{' '}
                             <Button
-                                onClick={() =>{this.changeColor(4)}}
-                                variant = {this.state.blueButtonId === 4 ? "warning": "outline-secondary"}
+                                variant = "outline-secondary"
                                 size="lg"
                                 style={{width: 120}}
                                 className="mx-3 mb-5">
@@ -89,4 +86,4 @@ class Buttons extends Component {
     }
 }
  
-export default Buttons;
+export default Main;
