f162f9dbaef3811b39463d837cfa2e3792d70314
Kelvin Yong <kyyong@up.edu.ph>
2021-05-24 21:57:59 +0800

    added encryption to password (no decryption for login, will fix later)

--- backend/routes/account-info.js
+++ backend/routes/account-info.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var app = express();
 var database = require('../config/database');
-var moment = require('moment');
+//var moment = require('moment');
 
 //get data from account-info table
 app.get('/account-info', (req,res) => {
@@ -21,12 +22,15 @@ app.get('/account-info', (req,res) => {
 });
 
 //to register user info in account_info table
-app.post('/user-registration', (req,res)=> {
+app.post('/user-registration', async (req,res)=> {
     
     const {username,email,password}=req.body
 
+    hashPass = await bcrypt.hash(password,10)
+    console.log(password+'\n'+hashPass)
+
     database.query("INSERT INTO account_info (username, email, password) VALUES (?,?,?)", 
-    [username, email, password], 
+    [username, email, hashPass], 
     (err, result) => {
         if(!err)
             res.send(result)
@@ -36,9 +40,11 @@ app.post('/user-registration', (req,res)=> {
 });
 
 //authentication for user-login
-app.post('/user-login', (req,res)=> {
+app.post('/user-login', async(req,res)=> {
 
     const {username,password}=req.body
+    hashPass = await bcrypt.compare(password,hash)
+    console.log(password+'\n'+hashPass)
 
     database.query("SELECT * FROM account_info WHERE username = ? AND password = ?", 
     [username, password], 
@@ -46,7 +52,7 @@ app.post('/user-login', (req,res)=> {
         // if(err){
         //     err.send({message: "Wrong username and/or password!"});
         // } 
-        if (result.length > 0) {
+        if (result.length > 0 && hashPass) {
             //console.log("Username and Password: ")
             //console.log(result)
             res.send(result)
