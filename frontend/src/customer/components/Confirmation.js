6288fea3322dfb6a336b92637234688a296a8337
daisybolivar <dsbolivar@up.edu.ph>
2021-06-10 03:21:11 +0800

    Fixed responsiveness

--- frontend/src/customer/components/Confirmation.js
+++ frontend/src/customer/components/Confirmation.js
@@ -35,10 +35,6 @@ const Container = styled.div`
     background-color: white;
     box-shadow: 3px 6px 5px 3px #d6d6d6;
     border-radius: 70px;
-    /* height: 800px;
-    width: 800px;
-    margin-top: -400px;
-    margin-left: -400px; */
     height: 80vh;
     width: 80vh;
     margin-top: -40vh;
@@ -53,33 +49,25 @@ const Container = styled.div`
     h1{
         margin-top: 5vh;
         font-weight: bolder;
-        /* font-size: 45px; */
         font-size: 4vh;
 
     }
 
     h2{
-        /* margin-top: -40px;
-        padding: 0px 70px; */
         margin-top: -1vh;
-        padding: -20vh 70px;
+        padding: -20vh 70vh;
         font-weight: lighter;
     }
 
     img{
-        /* margin-top: -90px;
-        width: 500px;
-        height: 500px; */
         margin-top: -10vh;
         width: 50vh;
         height: 50vh;
     }
 
     button{
-        margin-top: 25px;
+        margin-top: 2vh;
         font-family: "Work Sans";
-        /* width: 200px;
-        height: 90px; */
         width: 20vh;
         height: 10vh;
         border: none;
@@ -90,7 +78,6 @@ const Container = styled.div`
         cursor: pointer;
         color: black;
         background-color: #f9c91e;
-        /* box-shadow: 0px 14px 9px -15px rgba(0,0,0,0.25); */
         :hover{
             transform: translateY(2px)
         }
